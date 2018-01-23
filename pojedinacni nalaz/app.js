0744e09d7185e68a84a6f6f883174affcf66eb61
bojanKalanj <bojan.kalanj@gmail.com>
2018-01-23 17:20:07 +0100

    img modal

--- pojedinacni nalaz/app.js
+++ pojedinacni nalaz/app.js
@@ -19,6 +19,45 @@ window.addEventListener('scroll', function(){
     }
 })
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+// var slideIndex = 1;
+// showDivs(slideIndex);
+
+// function plusDivs(n) {
+//     showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//     var i;
+//     var x = document.getElementsByClassName("mySlides");
+//     if (n > x.length) {slideIndex = 1} 
+//     if (n < 1) {slideIndex = x.length} ;
+//     for (i = 0; i < x.length; i++) {
+//         x[i].style.display = "none"; 
+//     }
+//     x[slideIndex-1].style.display = "block"; 
+// }
+
 // console.log(document.querySelectorAll('.left img'));
 
 // document.querySelectorAll('.left img').forEach(function(image){
